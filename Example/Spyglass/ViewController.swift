062784e61d37ff07c20154d7bd159e69721422fc
Alexsander Akers <me@a2.io>
2016-09-02 21:10:55 +0200

    Initial commit

---
+++ Example/Spyglass/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Spyglass
+//
+//  Created by Alexsander Akers on 09/02/2016.
+//  Copyright (c) 2016 Alexsander Akers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
