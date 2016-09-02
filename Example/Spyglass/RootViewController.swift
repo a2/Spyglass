2079047aaaae5b9d81d866cc7a1563742a79b79a
Alexsander Akers <me@a2.io>
2016-09-02 22:12:23 +0200

    Create Spyglass class

---
+++ Example/Spyglass/RootViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RootViewController.swift
+//  Spyglass
+//
+//  Created by Alexsander Akers on 9/2/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UICollectionViewController {
+
+}
---
+++ Spyglass/Classes/Spyglass.swift
@@ -0,0 +1,19 @@
+//
+//  Spyglass.swift
+//  Spyglass
+//
+//  Created by Alexsander Akers on 09/02/2016.
+//  Copyright (c) 2016 Alexsander Akers. All rights reserved.
+//
+
+import UIKit
+
+public class Spyglass: NSObject, UINavigationControllerDelegate {
+    public func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationControllerOperation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return nil
+    }
+
+    public func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return nil
+    }
+}
