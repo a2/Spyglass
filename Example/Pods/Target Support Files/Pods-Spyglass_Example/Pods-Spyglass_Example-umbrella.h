2ab0154cdd13817ca651f431bf4dab56ee69f51b
Alexsander Akers <me@a2.io>
2016-09-02 21:12:27 +0200

    Commit pod installation artifacts

---
+++ Example/Pods/Target Support Files/Pods-Spyglass_Example/Pods-Spyglass_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Spyglass_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Spyglass_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-Spyglass_Tests/Pods-Spyglass_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Spyglass_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Spyglass_TestsVersionString[];
+
